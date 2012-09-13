fdd06a61b304b024b27f8569cdf27dc146545ed7
Anthony Mastrean <anthony.mastrean@gmail.com>
2012-09-13 09:08:13 -0400

    post-fix conditionals do not need x.nil? checks if you use the correct form

--- lib/albacore/nugetinstall.rb
+++ lib/albacore/nugetinstall.rb
@@ -32,21 +32,21 @@ class NuGetInstall
 		result = run_command "NuGet", params
 
 		failure_message = "Nuget Install for package #{@package} failed. See Build log for details."
-		fail_with_message failure_message if !result
+		fail_with_message failure_message unless result
 	end
 
 	def generate_params
-		fail_with_message 'A NuGet package must be specified.' if @package.nil?
+		fail_with_message 'A NuGet package must be specified.' unless @package
 
 		params = []
 		params << 'install'
 		params << package
-		params << "-Version #{version}" unless @version.nil?
-		params << "-OutputDirectory #{output_directory}" unless @output_directory.nil?
+		params << "-Version #{version}" if @version
+		params << "-OutputDirectory #{output_directory}" if @output_directory
 		params << "-ExcludeVersion" if @exclude_version
 		params << "-NoCache" if @no_cache
 		params << "-Prerelease" if @prerelease
-		params << "-Source #{build_package_sources}" unless @sources.nil? || @sources.empty?
+		params << "-Source #{build_package_sources}" if @sources unless @sources.empty?
 
 		merged_params = params.join(' ')
 	end
