720e59d2eb5a316ac7008f83ef0e74c1bfce3cd0
Anthony Mastrean <anthony.mastrean@gmail.com>
2012-10-23 00:35:58 -0400

    single quotes are fine on simple strings and parens are optional!

--- lib/albacore/config/netversion.rb
+++ lib/albacore/config/netversion.rb
@@ -1,23 +1,23 @@
 module Configuration
   module NetVersion
     def win_dir
-      @win_dir ||= ENV['windir'] || ENV['WINDIR'] || "C:/Windows"
+      @win_dir ||= ENV['windir'] || ENV['WINDIR'] || 'C:/Windows'
     end
    
     def get_net_version(netversion)
       case netversion
         when :net2, :net20
-          version = "v2.0.50727"
+          version = 'v2.0.50727'
         when :net35
-          version = "v3.5"
+          version = 'v3.5'
         when :net4, :net40
-          version = "v4.0.30319"
-		when :net45
-		  version = "v4.0.30319"
+          version = 'v4.0.30319'
+        when :net45
+          version = 'v4.0.30319'
         else
-          fail "#{netversion} is not a supported .net version"
+          fail "#{netversion} is not a supported .NET version"
       end
-      File.join(win_dir.dup, 'Microsoft.NET', 'Framework', version)
+      File.join win_dir.dup, 'Microsoft.NET', 'Framework', version
     end
   end
 end
