ad627215be1ae719a67bff212922c640fa6bc1ae
Henrik <henrik@haf.se>
2012-12-31 18:28:13 +0100

    adding requires

--- lib/albacore.rb
+++ lib/albacore.rb
@@ -2,5 +2,11 @@
 
 require 'albacore/version'
 
+require 'albacore/albasemver'
+require 'albacore/asminfo'
+require 'albacore/build'
+require 'albacore/nugets_restore'
+require 'albacore/restore_hint_paths'
+
 module Albacore
 end
