d8193aee074b3d296b6c8d3964dcea4f72ddd801
Henrik <henrik@haf.se>
2013-01-05 22:01:18 +0100

    adding test_runner

---
+++ lib/albacore/test_runner.rb
@@ -0,0 +1,51 @@
+# -*- encoding: utf-8 -*-
+
+require 'rake'
+require 'set'
+require 'albacore/paths'
+require 'albacore/cmd_config'
+require 'albacore/cross_platform_cmd'
+
+module Albacore
+  module TestRunner
+    class Cmd
+      include Logging
+      include CrossPlatformCmd
+      def initialize work_dir, executable, files
+        @work_dir, @executable = work_dir, executable
+        @parameters = files
+      end
+      def execute
+        sh @work_dir, make_command
+      end
+    end
+    class Config
+      include CmdConfig
+      attr_accessor :files
+    end
+    class Task
+      def initialize command_line
+        @command_line = command_line
+      end
+      def execute
+        @command_line.execute
+      end
+    end
+  end
+end
+
+def testrunner *args
+  args ||= []
+  
+  c = Albacore::TestRunner::Config.new
+  yield c
+
+  body = proc {
+    # Albacore::Paths.normalize_slashes p
+    command = Albacore::TestRunner::Cmd.new c.work_dir, c.exe, c.files
+      Albacore::TestRunner::Task.new(command).execute
+	  end
+  }
+
+  Rake::Task.define_task(*args, &body)
+end
