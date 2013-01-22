0b3bd5e5e7f12e287ae49d47935692ea8aca292c
Henrik <henrik@haf.se>
2013-01-22 19:23:23 +0100

    started adding module methods

---
+++ lib/albacore/module_methods.rb
@@ -0,0 +1,7 @@
+# -*- encoding: utf-8 -*-
+
+module Albacore
+  def self.configure 
+    
+  end
+end
---
+++ lib/albacore/nugets_pack.rb
@@ -0,0 +1,7 @@
+# -*- encoding: utf-8 -*-
+
+require 'rake'
+require 'albacore/paths'
+require 'albacore/cmd_config'
+require 'albacore/cross_platform_cmd'
+
