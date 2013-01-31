5c63d9156c87996724c222482928e8cc6211104e
Henrik <henrik@haf.se>
2013-01-31 15:20:00 +0100

    Writing of nugets_pack in testing mode

--- lib/albacore/nugets_pack.rb
+++ lib/albacore/nugets_pack.rb
@@ -35,30 +35,43 @@ module Albacore
         end
       end
     end
+
     class Package
       attr_accessor :metadata, :files
       def initialize
         @metadata = Metadata.new
         @files = []
       end
+      def add_file src, target, exclude
+        @files << OpenStruct.new(:src => src, :target => target, :exclude => exclude)
+      end
       def to_xml_builder
         md = Nokogiri::XML::Document.new @metadata.to_xml_builder.to_xml
         Nokogiri::XML::Builder.new(:encoding => 'UTF-8') do |x|
-          x.package('xmlns' => 'http://schemas.microsoft.com/packaging/2010/07/nuspec.xsd') {
+          x.package(:xmlns => 'http://schemas.microsoft.com/packaging/2010/07/nuspec.xsd') {
             x.metadata md.at_css("metadata").to_s
+            x.files {
+              @files.each do |f|
+                x.file(:src => f.src, :target => f.target, :exclude => f.exclude)
+              end
+            }  
           }
         end
+      end
+      def to_xml
+        to_xml_builder.to_xml
       end
     end
     class Cmd
       include CrossPlatformCmd
       def initialize work_dir, executable, out, opts
         #opts = Map.options(opts)
-        @executable = executable 
+        @executable = executable
         @work_dir   = work_dir
         @parameters = %W{pack -OutputDirectory #{out}}
       end
-      def execute
+      def execute nuspec_file
+        @parameters << nuspec_file
         sh make_command 
       end
     end
@@ -79,15 +92,27 @@ module Albacore
       def opts ; end
     end
     class Task
-      def initialize command_line
-        
+      def initialize command_line, config, file
+        @config = config
+        @file = file
         @command_line = command_line
       end
       def execute
-        # create nuspec::TODO
-        
+        filename = File.basename file
 
-        @command_line.execute
+        fpkg = File.join @config.out, filename
+        lib = File.join fpkg, 'lib'
+        FileUtils.mkdir_p lib
+
+        p = Package.new
+        p.add_file 'lib\\**'
+
+        nuspec = File.join fpkg, (filename + ".nuspec")
+        File.open(nuspec, 'w') do |io|
+          io.puts p.to_xml
+        end
+
+        @command_line.execute nuspec
       end
     end
   end
