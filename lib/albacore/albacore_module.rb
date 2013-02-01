04e67a8f16c7626f38cc7d2bdc9ed51a4379ed89
Henrik <henrik@haf.se>
2013-02-01 18:12:17 +0100

    retab

--- lib/albacore/albacore_module.rb
+++ lib/albacore/albacore_module.rb
@@ -4,20 +4,20 @@ require 'albacore/dsl'
 # The albacore module instance methods.
 module Albacore
   class << self
-		# Accessor for the Albacore application. Configuration
-		# and similar singleton values will be stored in this
-		# instance. Multiple calls will yield the same instance.
-		# Albacore::Application is thread-safe.
+    # Accessor for the Albacore application. Configuration
+    # and similar singleton values will be stored in this
+    # instance. Multiple calls will yield the same instance.
+    # Albacore::Application is thread-safe.
     def application
       @application ||= Albacore::Application.new
     end
   
-		# Defines a new task with all of what that entails:
-		# will call application.define_task. 
-	  def define_task *args, &block
-			# delegate to the application singleton
-			application.define_task *args, &block
-		end
+    # Defines a new task with all of what that entails:
+    # will call application.define_task. 
+    def define_task *args, &block
+      # delegate to the application singleton
+      application.define_task *args, &block
+    end
 
     # Set the global albacore logging level.
     def log_level= level
@@ -25,7 +25,7 @@ module Albacore
     end
 
     def subscribe event, &block
-    	event = event.to_sym unless event.is_a? Symbol
+      event = event.to_sym unless event.is_a? Symbol
       @events ||= {}
       @events[event] ||= Set.new
       @events[event].add block 
