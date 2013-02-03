3a1a6bc370957194c5e63c809f4e9fb8869a3a0b
Henrik <henrik@haf.se>
2013-02-03 19:03:35 +0100

    making events into a property to fix where using field before initializing it

--- lib/albacore/albacore_module.rb
+++ lib/albacore/albacore_module.rb
@@ -11,9 +11,9 @@ module Albacore
     def application
       @application ||= Albacore::Application.new
     end
-  
+
     # Defines a new task with all of what that entails:
-    # will call application.define_task. 
+    # will call application.define_task.
     def define_task *args, &block
       # delegate to the application singleton
       application.define_task *args, &block
@@ -24,16 +24,19 @@ module Albacore
       @level = level.is_a?(Albacore::Logging::LogLevel) ? level : Albacore::Logging::LogLevel.new(level)
     end
 
+    def events
+      @events ||= {}
+    end
+
     def subscribe event, &block
       event = event.to_sym unless event.is_a? Symbol
-      @events ||= {}
-      @events[event] ||= Set.new
-      @events[event].add block 
+      events[event] ||= Set.new
+      events[event].add block
     end
 
     def publish event, obj
-      if @events.member? event
-        @events[event].each { |m| m.call(obj) }
+      if events.member? event
+        events[event].each { |m| m.call(obj) }
       end
     end
   end
