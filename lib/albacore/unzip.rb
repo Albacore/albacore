bfe704034522b07bbf97cceea5d1ba214c38db78
Marcel Hoyer <mhoyer@pixelplastic.de>
2013-02-19 21:46:06 +0100

    implemented support for :force attribute in Unzip class to delete destination file before extracting

--- lib/albacore/unzip.rb
+++ lib/albacore/unzip.rb
@@ -21,6 +21,8 @@ class Unzip
       zip_f.each do |f|
         out_path = File.join(@destination, f.name)
         FileUtils.mkdir_p(File.dirname(out_path))
+
+        File.delete(out_path) if @force and File.file?(out_path)
         zip_f.extract(f, out_path) unless File.exist?(out_path)
       end
     end
--- spec/unzip_spec.rb
+++ spec/unzip_spec.rb
@@ -15,3 +15,37 @@ describe Unzip, "when providing configuration" do
     uz.force.should be_true
   end
 end
+
+describe Unzip, "when executing the task" do
+  let :uz do
+    zipped_file = mock('foo.txt')
+    zipped_file.stub(:name).and_return('foo.txt')
+
+    zip_file = mock('foo.zip')
+    zip_file.stub(:each).and_yield(zipped_file)
+    zip_file.stub(:extract)
+
+    Zip::ZipFile.stub!(:open).and_yield(zip_file)
+    FileUtils.stub(:mkdir_p)
+    File.stub!(:file?).and_return(true)
+
+    uz = Unzip.new
+    uz.file = 'foo.zip'
+    uz.destination = '/tmp'
+    uz
+  end
+
+  it "should delete the destinationfile if forced" do
+    File.should_receive(:delete).with('/tmp/foo.txt')
+
+    uz.force = true
+    uz.execute
+  end
+
+  it "should keep the destination file if not forced" do
+    File.should_not_receive(:delete)
+
+    uz.force = false
+    uz.execute
+  end
+end
