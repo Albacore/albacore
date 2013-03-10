04e7a3f3532d10540bede665c622cc80fb85a347
Oskar Gewalli <gewalli@gmail.com>
2013-03-10 14:25:52 +0100

    The projectlint tests should not depend on other spec's
    The testdata folder is used by other unrelated specs.

--- spec/projectlint_spec.rb
+++ spec/projectlint_spec.rb
@@ -1,18 +1,6 @@
 require 'albacore'
 require 'albacore/tasks/projectlint'
 
-describe "what methods are included by default" do
-  Albacore::Tasks::ProjectLint.new :projectlint_task do |c|
-    c.project = File.expand_path('../testdata/Project.fsproj', __FILE__)
-    c.ignores = [/\.sln$/, /\.config$/,]
-  end
-  it("should be defined") { Rake::Task.task_defined?(:projectlint_task).should be_true }
-  let(:projectlint) { Rake::Task[:projectlint_task] }
-  it("should report differences") { 
-    expect{ projectlint.invoke() }.to raise_error
-  }
-end
-
 class ProjectLintReturn
     attr_reader :failed, :failure_message
     def initialize(failed, message=nil)
