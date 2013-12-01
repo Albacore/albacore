ef1742a8b9311f60322de50d92770b3a0c91f59a
Anthony Mastrean <anthony.mastrean@gmail.com>
2013-12-01 18:00:11 -0500

    updating exec

--- lib/albacore/exec.rb
+++ lib/albacore/exec.rb
@@ -1,4 +1,4 @@
-require 'albacore/albacoretask'
+require "albacore/albacoretask"
 
 class Exec
   include Albacore::Task
@@ -6,13 +6,11 @@ class Exec
 
   def initialize
     super()
-    update_attributes Albacore.configuration.exec.to_hash
+    update_attributes(Albacore.configuration.exec.to_hash)
   end
     
   def execute
-    result = run_command "Exec"
-    
-    failure_message = 'Exec Failed. See Build Log For Detail'
-    fail_with_message failure_message if !result
+    result = run_command("Exec")
+    fail_with_message("Exec failed, see the build log for more details.") unless result
   end
 end
