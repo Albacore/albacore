23302102bbfc0435ddc2d824513d4f947cd2d9a1
Anthony Mastrean <anthony.mastrean@gmail.com>
2013-12-04 16:57:08 -0500

    Fixed the mstest task to the latest style
     * don't include an options when the parameters exists
     * don't build up the command line in this class, but in the RunCommand
     * don't test Albacore "system" stuff

--- spec/mstesttestrunner_spec.rb
+++ spec/mstesttestrunner_spec.rb
@@ -3,9 +3,8 @@ require 'albacore/mstesttestrunner'
 
 shared_context "mstest paths" do
   before :all do
-    @mstestpath = File.join(File.dirname(__FILE__), 'support', 'Tools', 'MSTest-2010', 'mstest.exe')
-    @test_assembly = File.join(File.expand_path(File.dirname(__FILE__)), 'support', 'CodeCoverage', 'mstest', 'TestSolution.MSTestTests.NET40.dll')
-    @test_option = "/test:Test /noisolation /noresults"
+    @command = File.join(File.dirname(__FILE__), 'support', 'Tools', 'MSTest-2010', 'mstest.exe')
+    @assembly = File.join(File.expand_path(File.dirname(__FILE__)), 'support', 'CodeCoverage', 'mstest', 'TestSolution.MSTestTests.NET40.dll')
   end
 end
 
@@ -13,62 +12,39 @@ describe MSTestTestRunner, "the command parameters for an mstest runner" do
   include_context "mstest paths"
 
   before :all do
-    mstest = MSTestTestRunner.new(@mstestpath)
-    mstest.assemblies @test_assembly
-    mstest.options @test_option
+    mstest = MSTestTestRunner.new()
+    mstest.command = @command
+    mstest.assemblies @assembly
     mstest.tests 'APassingTest', 'AFailingTest'
     
-    @command_parameters = mstest.get_command_parameters
+    @result = mstest.build_parameters.join(" ")
   end
     
   it "should not include the path to the command" do
-    @command_parameters.should_not include(@mstestpath)
+    @result.should_not include(@command)
   end
   
   it "should include the list of assemblies" do
-    @command_parameters.should include("/testcontainer:\"#{@test_assembly}\"")
+    @result.should include("/testcontainer:\"#{@assembly}\"")
   end
 
   it "should include the specific set of tests" do
-    @command_parameters.should include("/test:APassingTest /test:AFailingTest")
-  end
-  
-  it "should include the list of options" do
-    @command_parameters.should include(@test_option)
+    @result.should include("/test:APassingTest /test:AFailingTest")
   end
 end
-
-describe MSTestTestRunner, "the command line string for an mstest runner" do
-  include_context "mstest paths"
-
-  before :all do
-    mstest = MSTestTestRunner.new(@mstestpath)
-    mstest.assemblies @test_assembly
-    
-    @command_line = mstest.get_command_line
-    @command_parameters = mstest.get_command_parameters.join(" ")
-  end
-    
-  it "should start with the path to the command" do
-    @command_line.split(" ").first.should == @mstestpath
-  end
-  
-  it "should include the command parameters" do
-    @command_line.should include(@command_parameters)
-  end
-end
-
 
 describe MSTestTestRunner, "when configured correctly" do
   include_context "mstest paths"
 
   before :all do
-    mstest = MSTestTestRunner.new(@mstestpath)
+    mstest = MSTestTestRunner.new()
+    mstest.command = @command
     mstest.extend(FailPatch)
-    mstest.assemblies @test_assembly
+    mstest.assemblies @assembly
     mstest.log_level = :verbose
-    mstest.options "/noisolation /noresults"
+    mstest.parameters "/noisolation", "/noresults"
     mstest.tests "APassingTest"
+    mstest.no_logo
     mstest.execute
   end
   
@@ -81,12 +57,14 @@ describe MSTestTestRunner, "when configured correctly, but a test fails" do
   include_context "mstest paths"
 
   before :all do
-    mstest = MSTestTestRunner.new(@mstestpath)
+    mstest = MSTestTestRunner.new()
+    mstest.command = @command
     mstest.extend(FailPatch)
-    mstest.assemblies @test_assembly
+    mstest.assemblies @assembly
     mstest.log_level = :verbose
-    mstest.options "/noisolation /noresults"
+    mstest.parameters "/noisolation", "/noresults"
     mstest.tests "AFailingTest"
+    mstest.no_logo
     mstest.execute
   end
 
@@ -94,51 +72,4 @@ describe MSTestTestRunner, "when configured correctly, but a test fails" do
     $task_failed.should be_true
   end
 end
-
-describe MSTestTestRunner, "when using the configuration command and not providing a command in the intializer" do
-  include_context "mstest paths"
-
-  before :all do
-    Albacore.configure do |config|
-      config.mstest.command = "configured command"
-    end
-    @mstest = MSTestTestRunner.new
-  end
-
-  it "should use the configuration command" do
-    @mstest.command.should == "configured command"
-  end
-end
-
-describe MSTestTestRunner, "when the command has been set through configuration and providing a command in the intializer" do
-  include_context "mstest paths"
-
-  before :all do
-    Albacore.configure do |config|
-      config.mstest.command = "configured command"
-    end
-    @mstest = MSTestTestRunner.new("initializer command")
-  end
-
-  it "should use the initializer command" do
-    @mstest.command.should == "initializer command"
-  end
-end
-
-describe MSTestTestRunner, "when configuration has been provided" do
-  before :all do
-    Albacore.configure do |config|
-      config.mstest do |mstest|
-        mstest.assemblies = ["foo.dll", "bar.dll"]
-        mstest.options = ["/test"]
-      end
-    end
-
-    @mstest = MSTestTestRunner.new
-  end
-
-  it "should use the provided configuration" do
-    @mstest.assemblies.should == ["foo.dll", "bar.dll"]
-    @mstest.options.should == ["/test"]
-  end
-end
+  
