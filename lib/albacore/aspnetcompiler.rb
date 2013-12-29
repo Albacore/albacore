a68697ba1e73d96b11f77a089c9c25212b50ef06
Anthony Mastrean <anthony.mastrean@gmail.com>
2013-12-29 17:16:27 -0500

    aspnetcompiler

--- lib/albacore/aspnetcompiler.rb
+++ lib/albacore/aspnetcompiler.rb
@@ -10,6 +10,13 @@ class AspNetCompiler
   include Configuration::AspNetCompiler
   include SupportsLinuxEnvironment
 
+  attr_reader   :clean,
+                :delay_sign,
+                :fixed_names,
+                :force,
+                :updateable,
+                :debug
+
   attr_accessor :physical_path, 
                 :target_path, 
                 :virtual_path
@@ -17,13 +24,18 @@ class AspNetCompiler
   def initialize
     @virtual_path = "/"
     super()
-    update_attributes aspnetcompiler.to_hash
+    update_attributes(aspnetcompiler.to_hash)
   end
 
-  def execute
+  def execute    
+    result = run_command("AspNetCompiler", build_parameters)
+    fail_with_message("AspNetCompiler failed, see the build log for more details.") unless result
+  end
+
+  def build_parameters
     p = []
     p << "-v #{@virtual_path}" if @virtual_path
-    p << "-p #{format_path @physical_path}" if @physical_path
+    p << "-p #{format_path(@physical_path)}" if @physical_path
     p << "-c" if @clean
     p << "-delaysign" if @delay_sign
     p << "-fixednames" if @fixed_names
@@ -31,9 +43,7 @@ class AspNetCompiler
     p << "-u" if @updateable
     p << "-f" if @force
     p << format_path(@target_path) if @target_path
-    
-    result = run_command "AspNetCompiler", p
-    fail_with_message "AspNetCompiler failed, see the build log for more details." unless result
+    p
   end
   
   def updateable
---
+++ spec/aspnetcompiler_spec.rb
@@ -0,0 +1,76 @@
+require "spec_helper"
+require "albacore/aspnetcompiler"
+
+describe AspNetCompiler do
+  before :each do
+    @cmd = AspNetCompiler.new()
+    @cmd.extend(SystemPatch)
+    @cmd.extend(FailPatch)
+    @cmd.command = "aspnetcompiler"
+    @cmd.physical_path = "physical/path"
+    @cmd.target_path = "target/path"
+    @cmd.clean
+    @cmd.debug
+    @cmd.force
+    @cmd.fixed_names
+    @cmd.delay_sign
+    @cmd.updateable
+  end
+
+  describe AspNetCompiler, "when overriding all values" do
+    before :each do
+      @cmd.virtual_path = "virtual/path"
+      @cmd.execute
+    end
+
+    it "should use the command" do
+      @cmd.system_command.should include("aspnetcompiler")
+    end
+
+    it "should use the physical path" do
+      @cmd.system_command.should include("-p \"physical/path\"")
+    end
+
+    it "should use the target path" do
+      @cmd.system_command.should include("\"target/path\"")
+    end
+
+    it "should use default virtual path" do
+      @cmd.system_command.should include("-v virtual/path")
+    end
+
+    it "should be clean" do 
+      @cmd.system_command.should include("-c")
+    end
+
+    it "should delay sign" do 
+      @cmd.system_command.should include("-delaysign")
+    end
+
+    it "should use fixed names" do 
+      @cmd.system_command.should include("-fixednames")
+    end
+
+    it "should be updateable" do 
+      @cmd.system_command.should include("-u")
+    end
+
+    it "should force" do 
+      @cmd.system_command.should include("-f")
+    end
+
+    it "should debug" do 
+      @cmd.system_command.should include("-d")
+    end
+  end
+
+  describe AspNetCompiler, "when relying on defaults" do
+    before :each do
+      @cmd.execute
+    end
+
+    it "should use the default virtual path" do
+      @cmd.system_command.should include("-v /")
+    end
+  end
+end
