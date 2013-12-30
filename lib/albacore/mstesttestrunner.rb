ea76f122dc25de7e051a0dc4ebebf253ec7ee4c4
Anthony Mastrean <anthony.mastrean@gmail.com>
2013-12-30 15:05:58 -0500

    mstest

--- lib/albacore/mstesttestrunner.rb
+++ lib/albacore/mstesttestrunner.rb
@@ -14,24 +14,23 @@ class MSTestTestRunner
   
   def initialize()
     super()
-    update_attributes Albacore.configuration.mstest.to_hash
+    update_attributes(Albacore.configuration.mstest.to_hash)
   end
   
   def execute()
     result = run_command("MSTest", build_parameters)
     fail_with_message("MSTest failed, see the build log for more details.") unless result
   end  
-  
-  def no_logo
-    @no_logo = true
-  end
     
   def build_parameters
     p = []
-    p << @options if @options
-    p << @assemblies.map{ |asm| "/testcontainer:\"#{asm}\"" } if @assemblies
-    p << @tests.map{ |test| "/test:#{test}"} if @tests
+    p << @assemblies.map { |asm| "/testcontainer:\"#{asm}\"" } if @assemblies
+    p << @tests.map { |test| "/test:#{test}"} if @tests
     p << "/nologo" if @no_logo
     p
+  end
+  
+  def no_logo
+    @no_logo = true
   end
 end
--- spec/mstesttestrunner_spec.rb
+++ spec/mstesttestrunner_spec.rb
@@ -1,75 +1,31 @@
-require 'spec_helper'
-require 'albacore/mstesttestrunner'
+require "spec_helper"
+require "albacore/mstesttestrunner"
 
-shared_context "mstest paths" do
+describe MSTestTestRunner do
   before :all do
-    @command = File.join(File.dirname(__FILE__), 'support', 'Tools', 'MSTest-2010', 'mstest.exe')
-    @assembly = File.join(File.expand_path(File.dirname(__FILE__)), 'support', 'CodeCoverage', 'mstest', 'TestSolution.MSTestTests.NET40.dll')
+    @cmd = MSTestTestRunner.new()
+    @cmd.extend(SystemPatch)
+    @cmd.extend(FailPatch)
+    @cmd.command = "mstest"
+    @cmd.assemblies = ["a.dll", "b.dll"]
+    @cmd.tests = ["foo", "bar"]
+    @cmd.no_logo
+    @cmd.execute
+  end
+
+  it "should use the command" do
+    @cmd.system_command.should include("mstest")
+  end
+
+  it "should hide the startup banner" do
+    @cmd.system_command.should include("/nologo")
+  end
+
+  it "should test the assemblies" do
+    @cmd.system_command.should include("/testcontainer:\"a.dll\" /testcontainer:\"b.dll\"")
+  end
+
+  it "should run only the tests" do
+    @cmd.system_command.should include("/test:foo /test:bar")
   end
 end
-
-describe MSTestTestRunner, "the command parameters for an mstest runner" do
-  include_context "mstest paths"
-
-  before :all do
-    mstest = MSTestTestRunner.new()
-    mstest.command = @command
-    mstest.assemblies @assembly
-    mstest.tests 'APassingTest', 'AFailingTest'
-    
-    @result = mstest.build_parameters.join(" ")
-  end
-    
-  it "should not include the path to the command" do
-    @result.should_not include(@command)
-  end
-  
-  it "should include the list of assemblies" do
-    @result.should include("/testcontainer:\"#{@assembly}\"")
-  end
-
-  it "should include the specific set of tests" do
-    @result.should include("/test:APassingTest /test:AFailingTest")
-  end
-end
-
-describe MSTestTestRunner, "when configured correctly" do
-  include_context "mstest paths"
-
-  before :all do
-    mstest = MSTestTestRunner.new()
-    mstest.command = @command
-    mstest.extend(FailPatch)
-    mstest.assemblies @assembly
-    mstest.log_level = :verbose
-    mstest.parameters "/noisolation", "/noresults"
-    mstest.tests "APassingTest"
-    mstest.no_logo
-    mstest.execute
-  end
-  
-  it "should execute" do
-    $task_failed.should be_false
-  end
-end
-
-describe MSTestTestRunner, "when configured correctly, but a test fails" do
-  include_context "mstest paths"
-
-  before :all do
-    mstest = MSTestTestRunner.new()
-    mstest.command = @command
-    mstest.extend(FailPatch)
-    mstest.assemblies @assembly
-    mstest.log_level = :verbose
-    mstest.parameters "/noisolation", "/noresults"
-    mstest.tests "AFailingTest"
-    mstest.no_logo
-    mstest.execute
-  end
-
-  it "should fail" do
-    $task_failed.should be_true
-  end
-end
-  
