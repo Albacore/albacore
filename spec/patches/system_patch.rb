f5a0f6f843348cf26fbeb991902ba09659503abb
Anthony Mastrean <anthony.mastrean@gmail.com>
2014-01-05 15:54:10 -0500

    systempatch: always disable the system

--- spec/patches/system_patch.rb
+++ spec/patches/system_patch.rb
@@ -1,20 +1,8 @@
-module SystemPatch
-  attr_accessor :disable_system, :force_system_failure
-  
-  def initialize
-    @disable_system = false
-    @force_command_failure = false
-  end
-  
-  def system_command
-    @system_command
-  end
+module SystemPatch    
+  attr_accessor :system_command
   
   def system(cmd)
     @system_command = cmd
-    result = true
-    result = super(cmd) if !disable_system
-    return false if @force_system_failure
-    return result
+    return true
   end
 end
