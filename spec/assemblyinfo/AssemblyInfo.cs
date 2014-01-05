e5da40294294dfe02a78e2889b2f2ed082e1ca95
Anthony Mastrean <anthony.mastrean@gmail.com>
2014-01-05 18:05:18 -0500

    The AssemblyInfo.cs should be UTF-8... maybe

--- spec/assemblyinfo/AssemblyInfo.cs
+++ spec/assemblyinfo/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("TestSolution")]
-[assembly: AssemblyCopyright("Copyright ©  2009")]
+[assembly: AssemblyCopyright("Copyright Â©  2009")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
