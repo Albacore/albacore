38ae4b6bbfb11303691178e42ae7ae20feebb8ad
Henrik <henrik@haf.se>
2014-02-21 13:24:25 +0100

    LONG_VERSION no 'special' part

--- lib/albacore/tasks/versionizer.rb
+++ lib/albacore/tasks/versionizer.rb
@@ -35,7 +35,7 @@ module Albacore
         ENV['FORMAL_VERSION'] = "#{ XSemVer::SemVer.new(ver.major, ver.minor, revision).format "%M.%m.%p"}"
 
         # four-numbers version, useful if you're dealing with COM/Windows
-        ENV['LONG_VERSION'] = "#{ver.format("%M.%m.%p%s")}.0"
+        ENV['LONG_VERSION'] = "#{ver.format '%M.%m.%p'}.0"
         
         body = proc {
           Albacore.publish :build_version, OpenStruct.new(
