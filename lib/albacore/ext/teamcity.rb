0511a665c6d908d3457da2616b0fcc050e52ced5
Henrik <henrik@haf.se>
2014-02-28 19:05:22 +0100

    publishing build_number instead

--- lib/albacore/ext/teamcity.rb
+++ lib/albacore/ext/teamcity.rb
@@ -2,10 +2,13 @@ require 'albacore'
 
 module Albacore
   module Ext
+
     # The teamcity module writes appropriate build-script "interaction messages"
     # (see http://confluence.jetbrains.com/display/TCD7/Build+Script+Interaction+with+TeamCity#BuildScriptInteractionwithTeamCity-artPublishing)
     # to STDOUT.
+    #
     module TeamCity
+
       # Escaped the progress message
       # (see http://confluence.jetbrains.com/display/TCD7/Build+Script+Interaction+with+TeamCity#BuildScriptInteractionwithTeamCity-ServiceMessages)
       # The Unicode symbol escape is not implemented
@@ -17,17 +20,18 @@ module Albacore
       # | (vertical bar)                           ||
       # [ (opening bracket)                        |[
       # ] (closing bracket)                        |]
+      #
       def self.escape message
         message.gsub(/([\[|\]|\|'])/, '|\1').gsub(/\n/, '|n').gsub(/\r/, '|r')
       end
+
       def self.configure
         Albacore.subscribe :artifact do |artifact|
           ::Albacore.puts "##teamcity[publishArtifacts '#{artifact.location}']"
         end
         Albacore.subscribe :build_version do |version|
           # tell teamcity our decision
-          # TODO: use #build_number
-          ::Albacore.puts "##teamcity[buildNumber '#{version.build_version}']"
+          ::Albacore.puts "##teamcity[buildNumber '#{version.build_number}']"
         end
         Albacore.subscribe :progress do |p|
           # tell teamcity of our progress
@@ -42,13 +46,17 @@ module Albacore
           finish_progress p.message
         end
       end
+
       private
+
       PROGRESS_QUEUE = []
+
       # Starts a new progress block
       def self.start_progress(name)
         PROGRESS_QUEUE.push name
         ::Albacore.puts "##teamcity[progressStart '#{escape name}']"
       end
+
       # Finishes the progress block and all child progress blocks
       def self.finish_progress(name = '')
         loop do
