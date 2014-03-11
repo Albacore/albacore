44c94a80617e9796b3febcd5a40fd2350daba8fb
Henrik <henrik@haf.se>
2014-03-11 11:17:28 +0100

    adding Zippy tool: API for zipping folders

---
+++ lib/albacore/tools/zippy.rb
@@ -0,0 +1,54 @@
+# encoding: utf-8
+require 'zip'
+require "albacore"
+
+# https://github.com/aussiegeek/rubyzip/blob/master/samples/example_recursive.rb
+# This is a simple example which uses rubyzip to
+# recursively generate a zip file from the contents of
+# a specified directory. The directory itself is not
+# included in the archive, rather just its contents.
+#
+# Usage:
+# dir_to_zip = "/tmp/input"
+# out_file = "/tmp/out.zip"
+# zf = Zippy.new dir_to_zip, out_file
+# zf.write
+#
+# Or:
+# z = Zippy.new(directory_to_zip, output_file) { |f| f.include? 'html' }
+# z.write
+class Zippy
+
+  # Initialize with the directory to zip and the location of the output archive.
+  def initialize input_dir, out_file, &filter
+    @input_dir = input_dir.
+      gsub(/[\/\\]$/, '').
+      gsub(/\\/, '/')
+    @out_file = out_file
+    @filter = block_given? ? filter : lambda { |f| true }
+  end
+
+  # Zips the input directory.
+  def write
+    FileUtils.rm @out_file if File.exists? @out_file
+    in_progress "Writing archive #{@out_file} from #{@input_dir}" do
+      Zip::File.open @out_file, Zip::File::CREATE do |zipfile|
+        Dir["#{@input_dir}/**/**"].reject{ |f| f == @out_file || !@filter.call(f) }.each do |file|
+          progress "deflating #{file}"
+          zipfile.add(file.sub(@input_dir + '/', ''), file)
+        end
+      end
+	end
+  end
+  
+  private
+  def in_progress msg, &block
+    Albacore.publish :start_progress, OpenStruct.new(:message => msg)
+    yield
+    Albacore.publish :finish_progress, OpenStruct.new(:message => msg)
+  end
+
+  def progress msg
+    Albacore.publish :progress, OpenStruct.new(:message => msg)
+  end
+end
