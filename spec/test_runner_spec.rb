9eddeb85d7656ef5bb339fdcbf4bc59788806181
Henrik <henrik@haf.se>
2014-04-07 19:03:06 +0200

    fixes #102 and fixes #109

--- spec/test_runner_spec.rb
+++ spec/test_runner_spec.rb
@@ -35,11 +35,11 @@ end
 
 describe 'the order of which parameters are passed', ::Albacore::TestRunner::Config do
   subject do
-    cmd = ::Albacore::TestRunner::Config.new
-    cmd.files = 'a/b/c/file.dll'
-    cmd.exe   = 'test-runner.exe'
-    cmd.add_parameter '/TestResults=abc.xml'
-    cmd
+    config = ::Albacore::TestRunner::Config.new
+    config.files = 'a/b/c/file.dll'
+    config.exe   = 'test-runner.exe'
+    config.add_parameter '/TestResults=abc.xml'
+    config
   end
 
   let :params do
@@ -57,29 +57,63 @@ end
 
 describe ::Albacore::TestRunner::Cmd do
   subject do
-    cmd = ::Albacore::TestRunner::Cmd.new 'work_dir', 'run-tests.exe', %w[params go here], 'lib.tests.dll'
+    cmd = ::Albacore::TestRunner::Cmd.new 'work_dir', 'run-tests.exe', %w[params go here], 'a/b/c/lib.tests.dll'
     cmd.extend ShInterceptor
+    cmd.execute
     cmd
   end
 
-  it do
-    should respond_to :execute
-  end
-
   it 'should include the parameters when executing' do
-    subject.execute
-
     # the intersection of actual parameters with expected should eq expected
     (subject.parameters - (subject.parameters - %w|params go here|)).
       should eq(%w|params go here|)
   end
+
+  it 'should give the full path when executing' do
+    (subject.parameters - %w|params go here|).should eq(%w|a/b/c/lib.tests.dll|)
+  end
 end
 
 describe ::Albacore::TestRunner::Task do
-  subject do
-    ::Albacore::TestRunner::Task.new Map.new
+  let :config do
+    config = ::Albacore::TestRunner::Config.new
+    config.files = 'a/b/c/file.dll'
+    config.exe   = 'test-runner.exe'
+    config.add_parameter '/TestResults=abc.xml'
+    config
   end
+
+  subject do
+    ::Albacore::TestRunner::Task.new(config.opts)
+  end
+
   it do
     should respond_to :execute
+  end
+
+  def test_dir_exe hash
+    given = hash.first[0]
+    expected = hash.first[1]
+    subject.send(:handle_directory, given[0], given[1]) do |dir, exe|
+      dir.should eq(expected[0])
+      exe.should eq(expected[1])
+    end
+
+  end
+
+  it 'should handle relative handle_directory' do
+    test_dir_exe ['d.dll', 'e.exe'] => ['.', 'e.exe']
+  end
+
+  it 'should handle actual relative directories correctly' do
+    test_dir_exe ['a/d.dll', 'e.exe'] => ['a', '../e.exe']
+  end
+
+  it 'should handle negative dirs by getting current dir name' do
+    subject.send(:handle_directory, '../d.dll', 'e.exe') do |dir, exe|
+      dir.should eq('..')
+      # at this point, the exe file is just a dir in
+      exe.should =~ /\w+\/e\.exe/
+    end
   end
 end
