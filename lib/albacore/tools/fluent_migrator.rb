8a6d4861d17ce7d57f02885c25bda132fb7d16a9
Henrik <henrik@haf.se>
2014-09-03 09:51:50 +0200

    v2.0.2 - FluentMigrator tool, check existance

--- lib/albacore/tools/fluent_migrator.rb
+++ lib/albacore/tools/fluent_migrator.rb
@@ -67,6 +67,9 @@ module Albacore::Tools
       end
 
       def execute
+        Dir.chdir (opt.get(:work_dir, '.')) do
+          raise "Missing FluentMigrator at #{@executable}" unless File.exists? @executable
+        end
         system @executable, @parameters, :work_dir => opts.get(:work_dir)
       end
 
--- lib/albacore/version.rb
+++ lib/albacore/version.rb
@@ -1,3 +1,3 @@
 module Albacore
-  VERSION = "2.0.1"
+  VERSION = "2.0.2"
 end
