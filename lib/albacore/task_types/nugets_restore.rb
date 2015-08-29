dd4ceaeb53ce5b9ce79d15d7190fc92a79beaf9b
Oskar Gewalli <oskar@gewalli.se>
2015-08-29 12:08:51 +0200

    Simple way of using nuget gem

--- lib/albacore/task_types/nugets_restore.rb
+++ lib/albacore/task_types/nugets_restore.rb
@@ -3,6 +3,7 @@ require 'nokogiri'
 require 'albacore/paths'
 require 'albacore/cmd_config'
 require 'albacore/cross_platform_cmd'
+require 'albacore/task_types/nugets'
 require 'albacore/task_types/nugets_authentication'
 
 module Albacore
@@ -134,6 +135,10 @@ module Albacore
         readd  = AddSourceCmd.new exe, source, username, password
         remove.execute
         readd.execute
+      end
+
+      def nuget_gem_exe
+        @exe = Albacore::Nugets::find_nuget_gem_exe
       end
 
       def opts_for_pkgcfg pkg
