69f197893061ca2e82dd355997fe75d4f3c4b422
Oskar Gewalli <oskar@gewalli.se>
2015-08-31 10:09:40 +0200

    Test that nugets exe can be found in gem

---
+++ spec/nugets_find_gem_exe_spec.rb
@@ -0,0 +1,21 @@
+# encoding: utf-8
+
+require 'spec_helper'
+require 'albacore'
+require 'albacore/task_types/nugets'
+
+
+describe "when trying to find nuget exe in gem" do
+  
+  subject do 
+    Albacore::Nugets::find_nuget_gem_exe
+  end
+
+  it "should return path to the correct executable" do
+    expect(subject).to end_with('nuget.exe')
+  end
+
+  it "the path should point to something" do
+    expect(File.exists?( subject)).to be true
+  end
+end
