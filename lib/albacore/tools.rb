4cf029742a20d44796d41cb3940f3e8e2d33fbb5
Henrik Feldt <henrik@haf.se>
2016-05-23 08:36:41 +0200

    [tools] Interpret execution as UTF-8
    See
     - https://github.com/SuaveIO/suave/pull/439#issuecomment-220882201
     - https://github.com/imathis/octopress/pull/551

--- lib/albacore/tools.rb
+++ lib/albacore/tools.rb
@@ -1,3 +1,4 @@
+# Encoding: utf-8
 require 'albacore'
 require 'xsemver'
 
@@ -11,7 +12,7 @@ module Albacore::Tools
     last_tag = tags[-1]
     # second_last_tag = tags[-2] || `git rev-list --max-parents=0 HEAD`
     # logs = `git log --pretty=format:%s #{second_last_tag}..`.split(/\n/)
-    logs = `git log --pretty=format:%s #{last_tag}..`.split(/\n/)
+    logs = `git log --pretty=format:%s #{last_tag}..`.encode!('UTF-8').split(/\n/)
     "Release Notes:
 #{logs.inject('') { |state, line| state + "\n * #{line}" }}"
   end
