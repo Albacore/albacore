b62975b1e8e37eb06ce9d0e336ef10d0e054a373
Henrik Feldt <henrik@haf.se>
2016-06-06 21:39:50 +0200

    [dsl] fixes #192 #130

--- lib/albacore/dsl.rb
+++ lib/albacore/dsl.rb
@@ -12,18 +12,18 @@ module Albacore
     # a rake task type for outputting assembly versions
     def asmver *args, &block
       require 'albacore/task_types/asmver'
-      Albacore.define_task *args do
+      Albacore.define_task *args do |task_name, own_args|
         c = Albacore::Asmver::Config.new
-        yield c
+        yield c, own_args
         Albacore::Asmver::Task.new(c.opts).execute
       end
     end
 
     def asmver_files *args, &block
       require 'albacore/task_types/asmver'
-      Albacore.define_task *args do
+      Albacore.define_task *args do |task_name, own_args|
         c = Albacore::Asmver::MultipleFilesConfig.new
-        yield c
+        yield c, own_args
 
         c.configurations.each do |conf|
           trace { "generating asmver for #{conf}" }
@@ -36,9 +36,9 @@ module Albacore
     # with MsBuild
     def build *args, &block
       require 'albacore/task_types/build'
-      Albacore.define_task *args do
+      Albacore.define_task *args do |task_name, own_args|
         c = Albacore::Build::Config.new
-        yield c
+        yield c, own_args
 
         fail "unable to find MsBuild or XBuild" unless c.exe
         command = Albacore::Build::Cmd.new(c.work_dir, c.exe, c.parameters)
@@ -49,9 +49,9 @@ module Albacore
     # restore the nugets to the solution
     def nugets_restore *args, &block
       require 'albacore/task_types/nugets_restore'
-      Albacore.define_task *args do
+      Albacore.define_task *args do |task_name, own_args|
         c = Albacore::NugetsRestore::Config.new
-        yield c
+        yield c, own_args
 
         c.ensure_authentication! 
 
@@ -65,9 +65,9 @@ module Albacore
     # pack nugets
     def nugets_pack *args, &block
       require 'albacore/task_types/nugets_pack'
-      Albacore.define_task *args do
+      Albacore.define_task *args do |task_name, own_args|
         c = Albacore::NugetsPack::Config.new
-        yield c
+        yield c, own_args
         Albacore::NugetsPack::ProjectTask.new(c.opts).execute
       end
     end
@@ -76,9 +76,9 @@ module Albacore
     # tests with albacore
     def test_runner *args, &block
       require 'albacore/task_types/test_runner'
-      Albacore.define_task *args do
+      Albacore.define_task *args do |task_name, own_args|
         c = Albacore::TestRunner::Config.new
-        yield c
+        yield c, own_args
         Albacore::TestRunner::Task.new(c.opts).execute
       end
     end
@@ -86,9 +86,9 @@ module Albacore
     # Restore hint paths to registered nugets
     def restore_hint_paths *args, &block
       require 'albacore/tools/restore_hint_paths'
-      Albacore.define_task *args do
+      Albacore.define_task *args do |task_name, own_args|
         c = Albacore::RestoreHintPaths::Config.new
-        yield c
+        yield c, own_args
 
         t = Albacore::RestoreHintPaths::Task.new c
         t.execute
@@ -99,16 +99,16 @@ module Albacore
     def appspecs *args, &block
       if Albacore.windows?
         require 'albacore/cpack_app_spec'
-        Albacore.define_task *args do
+        Albacore.define_task *args do |task_name, own_args|
           c = ::Albacore::CpackAppSpec::Config.new
-          yield c
+          yield c, own_args
           ::Albacore::CpackAppSpec::Task.new(c.opts).execute
         end
       else
         require 'albacore/fpm_app_spec'
-        Albacore.define_task *args do
+        Albacore.define_task *args do |task_name, own_args|
           c = ::Albacore::FpmAppSpec::Config.new
-          yield c
+          yield c, own_args
           ::Albacore::FpmAppSpec::Task.new(c.opts).execute
         end
       end
@@ -118,9 +118,9 @@ module Albacore
     # with SqlPackage
     def sql_package *args, &block
       require 'albacore/task_types/sql_package'
-      Albacore.define_task *args do
+      Albacore.define_task *args do |task_name, own_args|
         c = Albacore::SqlPackage::Config.new
-        yield c
+        yield c, own_args
 
         fail "SqlPackage.exe is not installed.\nPlease download and install Microsoft SSDT: https://msdn.microsoft.com/en-us/library/mt204009.aspx\nAnd add the location of SqlPackage.exe to the PATH system varible." unless c.exe
 
@@ -133,9 +133,9 @@ module Albacore
     # with Sql
     def sql_cmd *args, &block
       require 'albacore/task_types/sql_cmd'
-      Albacore.define_task *args do
+      Albacore.define_task *args do |task_name, own_args|
         c = Albacore::Sql::Config.new
-        yield c
+        yield c, own_args
         Albacore::Sql::SqlTask.new(c.work_dir, c.opts).execute
       end
     end
@@ -144,9 +144,9 @@ module Albacore
     # with IsPackage
     def is_package *args, &block
       require 'albacore/task_types/is_package'
-      Albacore.define_task *args do
+      Albacore.define_task *args do |task_name, own_args|
         c = Albacore::IsPackage::Config.new
-        yield c
+        yield c, own_args
 
         fail "IsPackage.exe is not installed.\nPlease download and install Microsoft SSDT-BI: https://msdn.microsoft.com/en-us/library/mt674919.aspx\nAnd add the location of IsPackage.exe to the PATH system varible." unless c.exe
 
